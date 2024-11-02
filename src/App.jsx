7b70ff0919b304eb2120e0044aaa41334d4a675a
MikelDev <millymike599@gmail.com>
2024-11-02 22:28:23 +0100

    add Results component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 5,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />;
-      <UserInput />;
+      <UserInput userInput={userInput} onChange={handleChange} />;
+      <Result input={userInput} />
     </>
   );
 }
